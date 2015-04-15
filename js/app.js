ded1cbf9a281ddfbbefa2048d8f4024ff3096cb7
Pawel Bednarczyk <p.bednarczyk@icloud.com>
2015-04-15 20:52:02 +0200

    poprawki

--- js/app.js
+++ js/app.js
@@ -14,10 +14,20 @@
 		}
 	}
 
+	function getSolutionData(nNumber, lastNumber, body, way){
+
+		    var rotation = Math.floor(nNumber / body);
+    		var number = (way?(lastNumber + nNumber):(lastNumber - nNumber)) % body;
+    		if(number < 0) number += body;
+    		var rWord = getRotationWord(rotation);
+
+ 			return  {"rotation": rotation, "rWord": rWord, "way": way, "number": number};
+
+	}
+
 	var app = angular.module('app', []);
 	
 	app.controller('LockedController', function(){
-    	//this.solution = {};
     	
     	this.addSolution = function(){
 
@@ -28,35 +38,14 @@
 
     		// another informations
     		var body = 40;
-    		var start = true;
+    		var way = true;
 
     		// solution
     		var solution = {};
 
-    		//1
-    		var rotation = Math.floor(xnumber / body);
-    		var number = (0 + xnumber) % body;
-    		if(number < 0) number += body;
-    		var rWord = getRotationWord(rotation);
-
- 			solution[0] = {"rotation": rotation, "rWord": rWord, "way": (start?"prawo":"lewo"), "number": number};
-
- 			//2
-    		rotation = Math.floor(ynumber / body);
-			number = (number - ynumber) % body;
-    		if(number < 0) number += body;
-    		var rWord = getRotationWord(rotation);
- 			
- 			solution[1] = {"rotation": rotation, "rWord": rWord, "way": (start?"lewo":"prawo"), "number": number};
-
- 			//3
-    		rotation = Math.floor(znumber / body);
-			number = (number + znumber) % body;
-    		if(number < 0) number += body;
-			var rWord = getRotationWord(rotation);
-
- 			solution[2] = {"rotation": rotation, "rWord": rWord, "way": (start?"prawo":"lewo"), "number": number};
-
+    		solution[0] = getSolutionData(xnumber, 0, body, way);
+    		solution[1] = getSolutionData(ynumber, solution[0].number, body, !(solution[0].way));
+    		solution[2] = getSolutionData(znumber, solution[1].number, body, !(solution[1].way))
 
     		return this.solution = solution;
     	};
